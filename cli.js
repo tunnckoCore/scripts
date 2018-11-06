6f7159fab43f5fc7b13742489cf5f1f09c00007d
Charlike Mike Reagent <mameto2011@gmail.com>
2018-11-06 07:28:04 +0200

    feat: run update
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- cli.js
+++ cli.js
@@ -4,13 +4,9 @@
 
 const path = require('path');
 const proc = require('process');
-const esmLoader = require('esm');
 
 const pkg = require('./package.json');
-
-const esmRequire = esmLoader(module);
-
-const cli = esmRequire(path.join(__dirname, pkg.module)).default;
+const cli = require('./index');
 
 /* eslint-disable promise/always-return */
 
--- scripts.config.js
+++
@@ -1,9 +0,0 @@
-export const docs = ['docks --outfile .verb.md', 'verb'];
-
-const globs = 'src test scripts.config.js cli.js';
-export const lint = `eslint ${globs} --cache --fix --quiet --format codeframe`;
-
-export const test = 'nyc asia';
-
-export const precommit = [lint, test];
-export const commit = ['git status --porcelain', 'git add -A', 'gitcommit -sS'];
